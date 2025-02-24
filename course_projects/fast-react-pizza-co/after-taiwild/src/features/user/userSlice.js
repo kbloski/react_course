af63e8f330adb5b5db445a421bfb5a66030926c4
Kamil Błoński <kblonski02@gmail.com>
2025-02-24 15:04:10 +0100

    Add userStore to project

---
+++ course_projects/fast-react-pizza-co/after-taiwild/src/features/user/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    username: ''
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        updateName( state, actions) {
+            state.username = actions.payload;
+        }
+    }
+})
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
