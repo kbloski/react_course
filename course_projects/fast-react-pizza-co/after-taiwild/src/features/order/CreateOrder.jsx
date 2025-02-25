847fa8d2f6fbcd74a7269900b0a9282019e9ae4c
Kamil Błoński <kblonski02@gmail.com>
2025-02-25 22:41:37 +0100

    cleart card after order

--- course_projects/fast-react-pizza-co/after-taiwild/src/features/order/CreateOrder.jsx
+++ course_projects/fast-react-pizza-co/after-taiwild/src/features/order/CreateOrder.jsx
@@ -3,8 +3,9 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
-import { getCartItems } from '../cart/cartSlice';
+import { clearCart, getCartItems } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -101,13 +102,11 @@ export async function action({ request }) {
 
   if (Object.keys(errors).length > 0) return errors;
 
-  // If everything is okay, create new order and redirect
+  // DO no overuse
+  store.dispatch(clearCart())
 
-  // const newOrder = await createOrder(order);
-
-  // return redirect(`/order/${newOrder.id}`);
-
-  return null;
+  const newOrder = await createOrder(order);
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
