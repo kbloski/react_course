a4d48cbe4622478930f284d5d754ab4615211b76
Kamil Błoński <kblonski02@gmail.com>
2025-02-27 21:12:47 +0100

    Update

--- all-in-one/react-all/src/features/ReduxToolkit/counterSlice.ts
+++ all-in-one/react-all/src/features/ReduxToolkit/counterSlice.ts
@@ -1,5 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
+
+// createAsyncThunk
+export const fetchCounter = createAsyncThunk(
+    'counter/fetchCounter',
+    async (_, {rejectWithValue}) => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+        return response.json()
+    }
+)
+
+
+ 
+
+
+
+
+
+// Slice
 interface CounterState {
     counter: number
 }
@@ -18,9 +36,13 @@ export const counterSlice = createSlice({
         decrement: (state) => {
         state.counter -= 1
         },
-    }
+    },
 })
 
 export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
+
+function async(_: any, arg1: {}): import("@reduxjs/toolkit").AsyncThunkPayloadCreator<unknown, void, { state?: unknown; dispatch?: import("redux-thunk").ThunkDispatch<unknown, unknown, import("redux").UnknownAction>; extra?: unknown; rejectValue?: unknown; serializedErrorType?: unknown; pendingMeta?: unknown; fulfilledMeta?: unknown; rejectedMeta?: unknown }> {
+    throw new Error("Function not implemented.")
+}
--- course_projects/fast-react-pizza-co/after-taiwild/src/features/order/CreateOrder.jsx
+++ course_projects/fast-react-pizza-co/after-taiwild/src/features/order/CreateOrder.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
-import { clearCart, getCartItems } from '../cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCartItems, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -16,13 +17,17 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const username = useSelector((state) => state.user.username);
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
 
-  // const [withPriority, setWithPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCartItems);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
 
   if (!cart.length) return <EmptyCart />
 
@@ -30,6 +35,9 @@ function CreateOrder() {
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
       {/* <Form method="POST" action="/order/new"> */}
+
+      <button onClick={() => dispatch( fetchAddress())}>Get Posisiton</button>
+
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
@@ -66,8 +74,8 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">
             Want to yo give your order priority?
@@ -77,7 +85,7 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? 'Placing order....' : 'Order now'}
+            {isSubmitting ? 'Placing order....' : `Order now from ${totalPrice.toFixed(2)}€`}
           </Button>
         </div>
       </Form>
@@ -92,7 +100,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === 'true',
   };
 
   const errors = {};
